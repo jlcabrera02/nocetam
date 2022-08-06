53e0c27aab4feffd753cd6d5dfc9e00b6128ab45
Jose Luis Cabrera Aguirre <aj3amdua@gmail.com>
2022-08-06 14:58:44 -0500

    Guardar bitacoras

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const canvass = document.getElementById("canvas");
 
 const app = (function () {
   const canvas = d.createElement("canvas"),
-  canvasNota = d.createElement("canvas"),
-  contextNota = canvasNota.getContext("2d"),
+    canvasNota = d.createElement("canvas"),
+    contextNota = canvasNota.getContext("2d"),
     context = canvas.getContext("2d"),
     main = d.getElementById("main");
 
@@ -72,55 +72,69 @@ const app = (function () {
   };
 
   public.createNote = function (coor) {
-    canvasNota.width = 2000
-    canvasNota.height = 380
+    canvasNota.width = 2000;
+    canvasNota.height = 380;
 
     state.nombre = coor.nota;
 
     let time = new Date(coor.tiempo),
-    year = time.getFullYear(),
-    month = time.getMonth() + 1,
-    day = time.getDate(),
-    hour = time.getHours(),
-    minute = time.getMinutes();
+      year = time.getFullYear(),
+      month = time.getMonth() + 1,
+      day = time.getDate(),
+      hour = time.getHours(),
+      minute = time.getMinutes();
 
     contextNota.fillStyle = "rgb(255,255,255, 0.5)";
-    contextNota.fillRect(0,0,820,380);
+    contextNota.fillRect(0, 0, 820, 380);
     contextNota.font = `50pt sans-serif`;
     contextNota.fillStyle = "black";
     contextNota.fillText(`Latitud: ${coor.latitud}`, 5, 60);
     contextNota.fillText(`Longitud: ${coor.longitud}`, 5, 120);
     contextNota.fillText(`Elevación: ${coor.altitud} m`, 5, 180);
     contextNota.fillText(`Presición: ${coor.presicion} m`, 5, 240);
-    contextNota.fillText(`Tiempo: ${zfill(day, 2)}-${zfill(month, 2)}-${zfill(year, 4)} ${zfill(hour, 2)}:${zfill(minute, 2)}`, 5, 300);
+    contextNota.fillText(
+      `Tiempo: ${zfill(day, 2)}-${zfill(month, 2)}-${zfill(year, 4)} ${zfill(
+        hour,
+        2
+      )}:${zfill(minute, 2)}`,
+      5,
+      300
+    );
     contextNota.fillText(`Nota: ${coor.nota}`, 5, 360);
-    app.aplastarNota(canvasNota.toDataURL())
+    app.aplastarNota(canvasNota.toDataURL());
   };
-  
-  public.aplastarNota = function(urlImage){
+
+  public.aplastarNota = function (urlImage) {
     let nota = new Image();
     nota.src = urlImage;
-    
-    let pitagoras = (Math.sqrt(Math.pow(edit.swidth, 2) + Math.pow(edit.sheight, 2)))/6
-    
-    let relacionAspecto = 380/820,
-    newHeight = relacionAspecto*pitagoras;
- 
+
+    let pitagoras =
+      Math.sqrt(Math.pow(edit.swidth, 2) + Math.pow(edit.sheight, 2)) / 6;
+
+    let relacionAspecto = 380 / 820,
+      newHeight = relacionAspecto * pitagoras;
+
     nota.onload = function () {
-      context.drawImage(nota, 8, edit.sheight-(newHeight+5), pitagoras*2.4, newHeight)
-    }
-  }
+      context.drawImage(
+        nota,
+        8,
+        edit.sheight - (newHeight + 5),
+        pitagoras * 2.4,
+        newHeight
+      );
+    };
+  };
 
   public.generarImage = function () {
     let a = d.createElement("a");
     a.href = canvas.toDataURL("image/jpeg");
     a.download = state.nombre || "notecam";
-    a.click()
-  }
+    a.click();
+    guardarBitacora();
+  };
 
   return public;
 })();
-
 
 //Evento que dectecta el cambio si el input file tiene un archivo guardado
 d.addEventListener("change", (e) => {
@@ -156,13 +170,13 @@ d.addEventListener("click", (e) => {
   } else if (e.target.matches("#cleanNota")) {
     app.loadPicture(edit);
   } else if (e.target.matches("#masmenos")) {
-    $formCreateNote.altitud.value += "±" 
+    $formCreateNote.altitud.value += "±";
   }
 });
 
 //formulario que obtendra los datos de la nota
-$formCreateNote.addEventListener("submit", e => {
-  e.preventDefault()
+$formCreateNote.addEventListener("submit", (e) => {
+  e.preventDefault();
   let formData = {
     [e.target.latitud.name]: e.target.latitud.value,
     [e.target.longitud.name]: e.target.longitud.value,
@@ -170,31 +184,52 @@ $formCreateNote.addEventListener("submit", e => {
     [e.target.presicion.name]: e.target.presicion.value,
     [e.target.tiempo.name]: e.target.tiempo.value,
     [e.target.nota.name]: e.target.nota.value,
-  }
+  };
 
-  toggleModal("defaultModal", false)
+  toggleModal("defaultModal", false);
 
-  app.createNote(formData)
-})
-
+  app.createNote(formData);
+});
 
 //Devuelve numero 0 a izquierda si es necesario
 function zfill(number, width) {
   var numberOutput = Math.abs(number); /* Valor absoluto del número */
-  var length = number.toString().length; /* Largo del número */ 
-  var zero = "0"; /* String de cero */  
-  
+  var length = number.toString().length; /* Largo del número */
+  var zero = "0"; /* String de cero */
+
   if (width <= length) {
-      if (number < 0) {
-           return ("-" + numberOutput.toString()); 
-      } else {
-           return numberOutput.toString(); 
-      }
+    if (number < 0) {
+      return "-" + numberOutput.toString();
+    } else {
+      return numberOutput.toString();
+    }
   } else {
-      if (number < 0) {
-          return ("-" + (zero.repeat(width - length)) + numberOutput.toString()); 
-      } else {
-          return ((zero.repeat(width - length)) + numberOutput.toString()); 
-      }
+    if (number < 0) {
+      return "-" + zero.repeat(width - length) + numberOutput.toString();
+    } else {
+      return zero.repeat(width - length) + numberOutput.toString();
+    }
+  }
+}
+
+//peticion que me ayuda a saber cuantas personas lo usan al mes
+
+async function guardarBitacora(body) {
+  try {
+    const options = {
+      method: "POST",
+      body: JSON.stringify(body),
+    };
+
+    let res = await fetch(
+      "https://sembrandovida.herokuapp.com/api/notecam",
+      options
+    );
+
+    let json = await res.json();
+
+    console.log(json);
+  } catch (error) {
+    console.log(error);
   }
 }
